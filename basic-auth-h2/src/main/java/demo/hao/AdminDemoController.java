53de0f04a91f7cae8e712b5eb5fcda41c363fbe6
hao <hao.deng.m@gmail.com>
2021-08-18 11:36:35 +0200

    print out auth to view the details

--- basic-auth-h2/src/main/java/demo/hao/AdminDemoController.java
+++ basic-auth-h2/src/main/java/demo/hao/AdminDemoController.java
@@ -1,5 +1,6 @@
 package demo.hao;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminDemoController {
 
     @GetMapping
-    public String hi() {
-        return "Hi, admin";
+    public String hi(Authentication auth) {
+        return "Hi, " + auth;
     }
 }
--- basic-auth-h2/src/main/java/demo/hao/DemoController.java
+++ basic-auth-h2/src/main/java/demo/hao/DemoController.java
@@ -1,5 +1,6 @@
 package demo.hao;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     @GetMapping
-    public String hi() {
-        return "Hi, user";
+    public String hi(Authentication auth) {
+        return "Hi, " + auth;
     }
 
     @DeleteMapping("/del")
